b38151d4833709d5f819c025059f853dc2008a04
Elionai Gouveia <elionaigouveia@gmail.com>
2022-04-27 21:11:21 -0300

    Salvando pela primeira vez

---
+++ Swirl_Steps.R
@@ -0,0 +1,19 @@
+# Quando for rodar swirl use os comando abaixo sempre:
+#library("swirl")
+#swirl()
+
+# Digitar skip() permite pular a pergunta atual.
+# Digitar play() permite que você experimente com R por conta própria; redemoinho vai ignorar o que você faz...
+# ATÉ que você digite nxt() que irá recuperar a atenção do redemoinho.
+# Digitar bye() faz com que o redemoinho saia. Seu progresso será salvo.
+# Digitar main() retorna ao menu principal do swirl.
+# Digitar info() exibe essas opções novamente.
+
+#c() #vector
+
+#getwd()  # which directory your R session is using as its current working directory 
+#ls() # List all the objects in your local workspace
+
+
+#list.files() ; dir()  #List all the files in your working directory
+#args(name_da_fc)  #determine the arguments to function
